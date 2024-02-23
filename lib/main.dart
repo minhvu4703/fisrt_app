3f75934c497343d98c2016ed1b6cd182284a9d0a
minhvu4703 <minhvu4703@gmail.com>
2024-02-23 16:42:35 +0700

    23/02/2024 - Elevated Button - Collum - Container - Expanded - Flexible - Row - SizeBox - Stack - Test1

--- lib/main.dart
+++ lib/main.dart
@@ -5,49 +5,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 //Main
 void main() {
 //Khởi động app
   runApp(MaterialApp(
-    home: SafeArea(
-      child: Scaffold(
-        body: MyWidget()
-      )
-    ),
+    home: SafeArea(child: Scaffold(body: MyWidget())),
     debugShowCheckedModeBanner: false,
   ));
 }
-class MyWidget extends StatelessWidget{
+
+class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(50),
-      child: TextButton.icon(
-        onPressed: null,
-          style: TextButton.styleFrom(
-            // foregroundColor: Colors.black87,
-            // backgroundColor: Colors.white,
-            // // minimumSize: const Size(240, 120),
-            // padding: const EdgeInsets.all(20.0),
-            // shape: RoundedRectangleBorder(
-            //   borderRadius: BorderRadius.circular(0)
-            // ),
-            // elevation: 20,
-            // shadowColor: Colors.black87.withOpacity(0.5),
-            // side: const BorderSide(width: 5, color: Colors.lightGreenAccent),
-            disabledBackgroundColor: Colors.blueGrey,
-            disabledForegroundColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(0)
-            )
-          ),
-        icon: const Icon(Icons.add, size: 30,),
-        label: const Text('Click Here!!!', style: TextStyle(fontSize: 30),
-          ),
-        ),
-    );
+    return Text('Yeh!');
   }
-
 }
-
